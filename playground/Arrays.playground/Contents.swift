68aef060e1aa953afc4b49ebb7d74e99d04210f3
Mrkonxyz <mrkonz@odds.team>
2023-01-15 14:56:22 +0700

    init

---
+++ playground/Arrays.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var myFavoriteMovies = ["Avatar","Avenger","Iron man","Wenday",1] as [Any]
+
+//as -> casting
+//any -> any object
+
+myFavoriteMovies[0]
+myFavoriteMovies[1]
+myFavoriteMovies[2]
+myFavoriteMovies[4]
+
+var myString = ["Test","Test"]
+
+myString[0].uppercased()
+myString.count
---
+++ playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+//snake_case
+//camelCase
+
+//String
+var greeting = "Hello, "
+var userName = "james"
+userName.append("o")
+userName.lowercased()
+userName.uppercased()
+print(greeting+userName)
+
+// let is const
+//integer & double & float
+
+// integer
+let userAge = 50
+let myNunmer = 4
+userAge / myNunmer
+
+//double
+let userAgeD = 50.0
+let myNunmerD = 4.0
+userAgeD / myNunmerD
+
+//boolean
+var myBoolean = false
+myBoolean = true
+
+// ------- Part 2 -------
+
+let myString : String = "50"
+let anotherNumber : Int = 50
+
+//define
+let myVarible : String
+
+//initialization
+myVarible = "Sarun"
+myVarible.uppercased()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
